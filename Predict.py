eb0e8b637ccd48d93b6c70f71c2f03222958b13b
Tripp Gordon <jgordo17@uvm.edu>
2019-09-23 16:56:54 -0400

    through st 42 d4

--- Predict.py
+++ Predict.py
@@ -3,16 +3,33 @@ import numpy as np
 
 from knn import KNN
 
+# load dataset
 knn = KNN()
 knn.Load_Dataset('iris.csv')
 x = knn.data[:,0]
 y = knn.data[:,1]
-
+# slice dataset
 trainX = knn.data[::2,0:2]
 trainY = knn.target[::2]
 testX = knn.data[1::2,0:2]
 testY = knn.target[1::2]
 
+# train
+knn.Use_K_Of(15)
+knn.Fit(trainX,trainY)
+
+# test
+wrongCount = 0
+for i in range(len(testY)):
+    actualClass = testY[i]
+    prediction = knn.Predict(testX[i,0:2])
+    print(actualClass, prediction)
+    if actualClass != prediction:
+        wrongCount += 1
+print "Percent correct: ", (1 - float(wrongCount)/len(testY)) * 100, "%"
+
+
+# PLOT:
 colors = np.zeros((3,3),dtype='f')
 colors[0,:] = [1,0.5,0.5]
 colors[1,:] = [0.5,1,0.5]
@@ -26,6 +43,13 @@ plt.figure()
 for i in range(0,numItems/2):
     itemClass = int(trainY[i])
     currColor = colors[itemClass,:]
-    plt.scatter(trainX[i,0],trainX[i,1],facecolor=currColor)
+    plt.scatter(trainX[i,0],trainX[i,1],c=currColor,s=50,lw=2,edgecolor='black')
+
+for i in range(0,numItems/2):
+    itemClass = int(testY[i])
+    currColor = colors[itemClass,:]
+    prediction = int(knn.Predict(testX[i,:]))
+    edgeColor = colors[prediction,:]
+    plt.scatter(testX[i,0],testX[i,1],c=currColor,s=50,lw=2,edgecolor=edgeColor)
 
 plt.show()
